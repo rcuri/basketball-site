8cce76c12d8ad321b320362a77729955d7a31109
Rodrigo Curiel <rodrigocuriel95@gmail.com>
2018-09-10 13:14:36 -0700

    modified player and team model with more statistics for each player

--- teams/models.py
+++ teams/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Team(models.Model):
+    team_name = models.CharField(max_length=30, blank=True)
+    season = models.IntegerField(default=1900)
+
+class Player(models.Model):
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    player_name = models.CharField(max_length=200, blank=True)
+    position = models.CharField(max_length=5, blank=True)
+    height = models.CharField(max_length=5, blank=True)
+    weight = models.IntegerField(default=150)
+    birth_date = models.CharField(max_length=30, blank=True)
