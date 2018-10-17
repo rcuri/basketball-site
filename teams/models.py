22726efdc84076579ea2f40079044175b5243e17
Rodrigo Curiel <rodrigocuriel95@gmail.com>
2018-10-16 19:31:02 -0700

    added screenshots of website

--- teams/models.py
+++ teams/models.py
@@ -6,6 +6,9 @@ class Team(models.Model):
     team_name = models.CharField(max_length=30, blank=True)
     season = models.IntegerField(default=1900)
 
+    def __str__(self):
+        return self.team_name
+
 class Player(models.Model):
     team = models.ForeignKey(Team, on_delete=models.CASCADE)
     player_name = models.CharField(max_length=200, blank=True)
@@ -13,3 +16,6 @@ class Player(models.Model):
     height = models.CharField(max_length=5, blank=True)
     weight = models.IntegerField(default=150)
     birth_date = models.CharField(max_length=30, blank=True)
+
+    def __str__(self):
+        return self.player_name
